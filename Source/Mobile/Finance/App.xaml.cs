148c0321a4fd502d935552c29e774fcbb4303363
Kastney <kastney@outlook.com>
2024-04-21 21:42:10 -0300

    ➕ build - Create Finance MAUI project

---
+++ Source/Mobile/Finance/App.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Finance {
+    public partial class App : Application {
+        public App() {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ Source/Mobile/Finance/AppShell.xaml.cs
@@ -0,0 +1,7 @@
+﻿namespace Finance {
+    public partial class AppShell : Shell {
+        public AppShell() {
+            InitializeComponent();
+        }
+    }
+}
