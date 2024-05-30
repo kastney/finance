8478f32b67f87761f9121566ef0d238c2d95e21b
Kastney <kastney@outlook.com>
2024-05-30 18:39:20 -0300

    🐛 fix - Loop overflow fix

--- Source/Mobile/Finance/Pages/Historic/HistoricPage.xaml.cs
+++ Source/Mobile/Finance/Pages/Historic/HistoricPage.xaml.cs
@@ -1,5 +1,3 @@
-using DevExpress.Maui.Editors;
-
 namespace Finance.Pages.Historic;
 
 public partial class HistoricPage : ContentPage {
@@ -26,12 +24,6 @@ public partial class HistoricPage : ContentPage {
     private async void BackButtom_Clicked(object sender, EventArgs e) {
         if(viewModel.CanBack()) {
             await viewModel.NavigationBack();
-        }
-    }
-
-    private void DXCalendar_DisableDate(object sender, DisableDateEventArgs e) {
-        if(e.Date.Year == 2024 && e.Date.Month == 5 && e.Date.Day == 15) {
-            e.IsDisabled = true;
         }
     }
 }
